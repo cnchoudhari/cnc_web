02c20e8a65feae6b6abfed9e5d35a3266a740809
Chetan Choudhari <108466081+cnchoudhari@users.noreply.github.com>
2023-03-01 07:11:40 +0000

    Initial commit

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask ,render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
-    return render_template('home.html')
+  return render_template('home.html')
 
 
 if __name__ == '__main__':
